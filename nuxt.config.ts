2f3c36e561ff43925c1b8dd2aca9521686be06b9
[SKP]김민경 <mk.kim109@sk.com>
2025-01-16 02:29:28 +0900

    장바구니 상태 정보를 저장하기 위해 pinia를 추가하여 store 생성

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "nuxt-svgo", "@nuxt/image", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/content", "nuxt-svgo", "@nuxt/image", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
   css: ["@/assets/css/main.css", "@/assets/css/details.css"],
   components: true,
   content: {
